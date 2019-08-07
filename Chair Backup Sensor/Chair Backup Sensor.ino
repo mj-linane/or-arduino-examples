b15d2ff8cee54ceb4845b55b386f2d3783e8b253
MJ Linane <mj@mjlinane.com>
2019-08-07 11:51:38 -0400

    moved functions in order

--- Chair Backup Sensor/Chair Backup Sensor.ino
+++ Chair Backup Sensor/Chair Backup Sensor.ino
@@ -32,85 +32,18 @@ int readIndex = 0;           // the index of the current reading
 int total = 0;               // the running total
 int average = 0;             // the average
 
-// Function to report range output
-void indicateRange(int inches)
+// Function to calculate sensor microseconds to Inches
+long microsecondsToInches(long microseconds)
 {
-
-  if (inches >= 70)
-  {
-    digitalWrite(13, HIGH);
-    digitalWrite(12, LOW);
-    noTone(2);
-  }
-  else if (inches >= 40 && inches <= 69)
-  {
-    digitalWrite(12, HIGH);
-    digitalWrite(13, LOW);
-    noTone(2);
-  }
-  else if (inches >= 15 && inches <= 39)
-  {
-    tone(2, 65);
-    digitalWrite(12, LOW);
-    digitalWrite(13, LOW);
-  }
-  else
-  {
-    tone(2, 250, 50);
-    digitalWrite(12, LOW);
-    digitalWrite(13, LOW);
-  }
+  // According to Parallax's datasheet for the PING))), there are 73.746
+  // microseconds per inch (i.e. sound travels at 1130 feet per second).
+  // This gives the distance travelled by the ping, outbound and return,
+  // so we divide by 2 to get the distance of the obstacle.
+  // See: http://www.parallax.com/dl/docs/prod/acc/28015-PING-v1.3.pdf
+  return microseconds / 74 / 2;
 }
 
-// Add to rangeArray
-void addReadingToRangeArray(int reading)
-{
-  // Collect data in datasheet
-  // subtract the last reading:
-  total = total - rangeArray[readIndex];
-  // read from the sensor:
-  rangeArray[readIndex] = reading;
-  // add the reading to the total:
-  total = total + rangeArray[readIndex];
-  // advance to the next position in the array:
-  readIndex = readIndex + 1;
-
-  // if we're at the end of the array...
-  if (readIndex >= numReadings)
-  {
-    // ...wrap around to the beginning:
-    readIndex = 0;
-  }
-}
-
-// Function to Calculate Average
-int rangeArrayCalculateAverage()
-{
-  return total / numReadings;
-}
-
-// Function to Calculate Mode
-int rangeArrayCalculateMode()
-{
-  int modeguess = 0; // unlikely number.
-  int count = 0;
-  int valmax = 0;
-
-  for (int i = 0; i < numReadings; i++)
-  {
-    if (rangeArray[i] == modeguess)
-    {
-      count++;
-    }
-    Serial.print(modeguess) if (count > valmax)
-    { // Check to see if there is a new max
-      modeguess = rangeArray[i];
-      valmax = count;
-    }
-  }
-  return modeguess;
-}
-
+// Function to read the sensors
 int readSensors()
 {
   // establish variables for duration of the ping, and the distance result
@@ -137,6 +70,85 @@ int readSensors()
 
   delay(1); // delay in between reads for stability
   return inchesRange;
+}
+
+// Add individual ping data to rangeArray
+void addReadingToRangeArray(int reading)
+{
+  // Collect data in datasheet
+  // subtract the last reading:
+  total = total - rangeArray[readIndex];
+  // read from the sensor:
+  rangeArray[readIndex] = reading;
+  // add the reading to the total:
+  total = total + rangeArray[readIndex];
+  // advance to the next position in the array:
+  readIndex = readIndex + 1;
+
+  // if we're at the end of the array...
+  if (readIndex >= numReadings)
+  {
+    // ...wrap around to the beginning:
+    readIndex = 0;
+  }
+}
+
+// Function to Calculate Range Array Average
+int rangeArrayCalculateAverage()
+{
+  return total / numReadings;
+}
+
+// Function to Calculate Range Array Mode
+int rangeArrayCalculateMode()
+{
+  int modeguess = 0; // unlikely number.
+  int count = 0;
+  int valmax = 0;
+
+  for (int i = 0; i < numReadings; i++)
+  {
+    if (rangeArray[i] == modeguess)
+    {
+      count++;
+    };
+    Serial.print(modeguess);
+    if (count > valmax)
+    { // Check to see if there is a new max
+      modeguess = rangeArray[i];
+      valmax = count;
+    }
+  }
+  return modeguess;
+}
+
+// Function to report range output to components
+void indicateRange(int inches)
+{
+  if (inches >= 70)
+  {
+    digitalWrite(13, HIGH);
+    digitalWrite(12, LOW);
+    noTone(2);
+  }
+  else if (inches >= 40 && inches <= 69)
+  {
+    digitalWrite(12, HIGH);
+    digitalWrite(13, LOW);
+    noTone(2);
+  }
+  else if (inches >= 15 && inches <= 39)
+  {
+    tone(2, 65);
+    digitalWrite(12, LOW);
+    digitalWrite(13, LOW);
+  }
+  else
+  {
+    tone(2, 250, 50);
+    digitalWrite(12, LOW);
+    digitalWrite(13, LOW);
+  }
 }
 
 // Setup the arduino
@@ -180,22 +192,4 @@ void loop()
   Serial.print(range);
   Serial.print("in, ");
   Serial.println();
-}
-
-long microsecondsToInches(long microseconds)
-{
-  // According to Parallax's datasheet for the PING))), there are 73.746
-  // microseconds per inch (i.e. sound travels at 1130 feet per second).
-  // This gives the distance travelled by the ping, outbound and return,
-  // so we divide by 2 to get the distance of the obstacle.
-  // See: http://www.parallax.com/dl/docs/prod/acc/28015-PING-v1.3.pdf
-  return microseconds / 74 / 2;
-}
-
-long microsecondsToCentimeters(long microseconds)
-{
-  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
-  // The ping travels out and back, so to find the distance of the object we
-  // take half of the distance travelled.
-  return microseconds / 29 / 2;
 }
